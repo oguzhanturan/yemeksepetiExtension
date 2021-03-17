61974cf24eef3d7eb69b361a67bd03296bddfae6
Oguzhan <oguzhanturan@yahoo.com>
2021-03-17 14:36:40 +0300

    deleted test files

--- caller.js
+++
@@ -1,7 +0,0 @@
-var para = document.createElement("p");
-var node = document.createTextNode("Gizle");
-para.appendChild(node);
-
-var element = document.getElementsByClassName("restaurant-main-info")[0]
-element.appendChild(para);
-
